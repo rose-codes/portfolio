904c44aad4f02057ff9916d4d398ec270e93166b
Rose Le <rosehle@protonmail.com>
2023-12-05 14:01:21 -0800

    Create contact and submission on client and server

---
+++ components/contact.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import { SectionHeading } from "./section-heading";
+import { FaPaperPlane } from "react-icons/fa";
+import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
+import { sendEmail } from "@/actions/sendEmail";
+
+export const Contact = () => {
+  const { ref } = useSectionInView("Contact");
+
+  return (
+    <motion.section
+      ref={ref}
+      id="contact"
+      className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center"
+      initial={{
+        opacity: 0,
+      }}
+      whileInView={{
+        opacity: 1,
+      }}
+      transition={{
+        duration: 1,
+      }}
+      viewport={{
+        once: true,
+      }}
+    >
+      <SectionHeading>Contact me</SectionHeading>
+      <p className="text-gray-700 -mt-5">
+        Please contact me directly via{" "}
+        <a className="underline" href="mailto:rosehle33@gmail.com">
+          email
+        </a>{" "}
+        or through this form.
+      </p>
+      <form
+        className="mt-10 flex flex-col"
+        action={async (formData) => {
+          console.log("Running on client");
+          console.log(formData.get("senderEmail"));
+          console.log(formData.get("message"));
+          await sendEmail(formData);
+        }}
+      >
+        <input
+          className="h-14 rounded-lg borderBlack px-4 active:border-black focus-visible:!outline-black focus-visible:!outline focus-visible:!outline-1"
+          name="senderEmail"
+          type="email"
+          placeholder="Your email"
+          required
+          maxLength={500}
+        />
+        <textarea
+          className="h-52 my-3 rounded-lg borderBlack p-4 focus-visible:!outline-black focus-visible:!outline  focus-visible:!outline-1"
+          name="message"
+          placeholder="Your message"
+          required
+          maxLength={500}
+        />
+        <button
+          type="submit"
+          className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105"
+        >
+          Submit{" "}
+          <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />
+        </button>
+      </form>
+    </motion.section>
+  );
+};
