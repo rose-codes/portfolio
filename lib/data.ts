82719230df2ccda65431be1853bf0294febe52aa
Rose Le <rosehle@protonmail.com>
2023-12-04 17:46:57 -0800

    Modify timeline experiences

--- lib/data.ts
+++ lib/data.ts
@@ -2,6 +2,9 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
+import { FaCode } from "react-icons/fa";
+import { DiJavascript } from "react-icons/di"
+import { FiPenTool } from "react-icons/fi"
 import rxRosterImg from "@/public/rxRoster.png";
 import whenWomenImg from "@/public/whenWomen.png";
 
@@ -34,28 +37,60 @@ export const links = [
 
 export const experiencesData = [
     {
-        title: "Graduated bootcamp",
-        location: "Remote",
+        title: "Full-Stack Developer",
+        location: "San Diego, CA",
         description:
-        "I graduated after from Ada Developers Academy where I learned Computer Science and Software Engineering Fundamentals.",
-        icon: React.createElement(LuGraduationCap),
-        date: "2023",
+        "I'm now a full-stack developer working on personal projects. My stack includes React, Next.js, TypeScript, Tailwind, and MongoDB. I'm open to full-time opportunities.",
+        icon: React.createElement(FaReact),
+        date: "2023 - present",
+    },
+    {
+        title: "Full-Stack Developer, Technical Team Lead",
+        location: "San Diego, CA",
+        description:
+        "I served as a full-stack developer and technical team lead for a Hackathon Project. We were placed in the top 20 out of 64 projects and won the Outstanding Social Impact Award ",
+        icon: React.createElement(FaReact),
+        date: "2023 - present",
     },
     {
         title: "Cloud Security - Full-Stack Software Engineer Intern",
         location: "Remote",
         description:
-        "I worked as a full-stack software engineer intern for the Cloud Security team.",
-        icon: React.createElement(CgWorkAlt),
+        "I worked as a full-stack software engineer intern for the Cloud Security team at a software development tools company.",
+        icon: React.createElement(CgWorkAlt), //cannot use component alone in TS file (but can in TSX file)
         date: "2023",
     },
     {
-        title: "Full-Stack Developer",
+        title: "Graduated Bootcamp",
+        location: "Remote",
+        description:
+        "I completed a Software Engineering Certificate and graduated from Ada Developers Academy where I learned Computer Science and Software Engineering Fundamentals.",
+        icon: React.createElement(LuGraduationCap),
+        date: "2022-2023",
+    },
+    {
+        title: "UI Designer Student",
+        location: "Remote",
+        description:
+        "I participated in a UI course to learn UI principles and utilizing Figma to create wireframes.",
+        icon: React.createElement(FiPenTool), //cannot use component alone in TS file (but can in TSX file)
+        date: "2022",
+    },
+    {
+        title: "JavaScript Developer Student",
+        location: "Remote",
+        description:
+        "I participated in a bootcamp where I developed projects using JavaScript and React.",
+        icon: React.createElement(DiJavascript), //cannot use component alone in TS file (but can in TSX file)
+        date: "2021-2022",
+    },
+    {
+        title: "Full-Stack Student (Self-Taught)",
         location: "San Diego, CA",
         description:
-        "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, and MongoDB. I'm open to full-time opportunities.",
-        icon: React.createElement(FaReact),
-        date: "2023 - present",
+        "I self-taught myself JavaScript and built mini projects using HTML and CSS.",
+        icon: React.createElement(FaCode), //cannot use component alone in TS file (but can in TSX file)
+        date: "2021-2022",
     },
 ] as const;
 
