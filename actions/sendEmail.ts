45a00cb32dcffef87de732982690de6c7363f0b4
Rose Le <rosehle@protonmail.com>
2023-12-05 15:49:32 -0800

    Create sendEmails helper Create validation for form Create validation for errors

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,9 +1,39 @@
 "use server";
 
-export const sendEmail = async (formData: FormData) => {
-    "use server";
+import { Resend } from "resend"
+import { validateString } from "@/lib/utils";
+import { getErrorMessage } from "@/lib/utils";
 
-    console.log("Running on server");
-    console.log(formData.get("senderEmail"));
-    console.log(formData.get("message"));
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+
+export const sendEmail = async (formData: FormData) => {
+    const senderEmail = formData.get('senderEmail')
+    const message = formData.get('message')
+
+    // server-side verification
+    if (!validateString(senderEmail, 500)) {
+        return {
+            error: "Invalid sender email"
+        }
+    }
+    if (!validateString(message, 5000)) {
+        return {
+            error: "Invalid message"
+        }
+    }
+    try {
+
+        await resend.emails.send({
+            from: "Contact Form <onboarding@resend.dev>",
+            to: 'rosehle33@gmail.com',
+            subject: 'Message from contact form',
+            text: message as string,
+            reply_to: senderEmail as string
+        })
+    } catch (error: unknown) {
+        return {
+            error: getErrorMessage(error)
+        };
+    }
 };
