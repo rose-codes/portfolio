f859b740daf32dc35413c12297f2a2773f27d53b
Rose Le <rosehle@protonmail.com>
2023-12-06 15:55:07 -0800

    Set up Tailwind to theme switch using class

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="!scroll-smooth">
-      <body className="bg-gray-50 text-gray-950 relative pt-28 sm:pt-36">
+      <body className="bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90 ">
         <div className="bg-[#fecdd3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
         <div className="bg-[#ffedd5] absolute top-[-6rem] -z-10 left-[11rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
         <ActiveSectionContextProvider>
--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -11,9 +11,11 @@ export const ThemeSwitch = () => {
     if (theme === "light") {
       setTheme("dark");
       window.localStorage.setItem("theme", "dark");
+      document.documentElement.classList.add("dark");
     } else {
       setTheme("light");
       window.localStorage.setItem("theme", "light");
+      document.documentElement.classList.remove("dark");
     }
   };
 
@@ -22,8 +24,12 @@ export const ThemeSwitch = () => {
 
     if (localTheme) {
       setTheme(localTheme);
+      if (localTheme === "dark") {
+        document.documentElement.classList.add("dark");
+      }
     } else if (window.matchMedia("(prefers-color-scheme: dark").matches) {
       setTheme("dark");
+      document.documentElement.classList.add("dark");
     }
   }, []);
   return (
