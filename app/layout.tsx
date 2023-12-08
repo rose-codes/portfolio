48769a6c63da0a37493e16cbaa527917d574e14b
Rose Le <rosehle@protonmail.com>
2023-12-08 14:31:37 -0800

    Add responsiveness to project section Fix layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
     <html lang="en" className="!scroll-smooth">
       <body className="bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90 ">
         <div className="bg-[#fecdd3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#fb7185]/20"></div>
-        <div className="bg-[#ffedd5] absolute top-[-6rem] -z-10 left-[11rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#f97316]/20"></div>
+        <div className="bg-[#ffedd5] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#f97316]/20"></div>
         <ThemeContextProvider>
           <ActiveSectionContextProvider>
             <Header />
