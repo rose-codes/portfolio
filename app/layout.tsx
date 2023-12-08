7a705e47337f2ac669aa3415a864c0e6c3a1130c
Rose Le <rosehle@protonmail.com>
2023-12-07 19:19:47 -0800

    Create theme-context and add to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Header } from "@/components/header";
 import { Toaster } from "react-hot-toast";
 import { Footer } from "@/components/footer";
 import { ThemeSwitch } from "@/components/theme-switch";
+import { ThemeContextProvider } from "@/context/theme-context";
 
 export const metadata = {
   title: "Rose Le | Portfolio",
@@ -21,13 +22,15 @@ export default function RootLayout({
       <body className="bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90 ">
         <div className="bg-[#fecdd3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#fb7185]/20"></div>
         <div className="bg-[#ffedd5] absolute top-[-6rem] -z-10 left-[11rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#f97316]/20"></div>
-        <ActiveSectionContextProvider>
-          <Header />
-          {children}
-          <Footer />
-          <Toaster position="top-right" />
-        </ActiveSectionContextProvider>
-        <ThemeSwitch />
+        <ThemeContextProvider>
+          <ActiveSectionContextProvider>
+            <Header />
+            {children}
+            <Footer />
+            <Toaster position="top-right" />
+          </ActiveSectionContextProvider>
+          <ThemeSwitch />
+        </ThemeContextProvider>
       </body>
     </html>
   );
